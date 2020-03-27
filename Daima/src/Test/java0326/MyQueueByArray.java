61f256a10129429d140284b905aa9c5816e8c6bd
worldgo400 <1130447609@qq.com>
2020-03-27 11:12:28 +0800

    27

---
+++ Daima/src/Test/java0326/MyQueueByArray.java
@@ -0,0 +1,38 @@
+package Test.java0326;
+
+public class MyQueueByArray {
+    public int[]array=new int[100];
+    private int head=0;
+    private int tail=0;
+    private int size=0;
+    public void offer(int val) {
+        if (size == array.length) {
+            return;
+        }
+        array[tail] = val;
+        tail++;
+        if (tail >= array.length) {
+            tail = 0;
+        }
+        size++;
+    }
+    public Integer poll(){
+        if (size==0){
+            return null;
+        }
+        Integer ret=array[head];
+        head=head++;
+        if (head>=array.length){
+            head=0;
+        }
+        size--;
+        return ret;
+    }
+    public Integer peek(){
+        if (size==0){
+            return null;
+        }
+        return array[head];
+    }
+
+}
---
+++ Daima/src/Test/java0326/MyQueueByLinkedList.java
@@ -0,0 +1,44 @@
+package Test.java0326;
+
+public class MyQueueByLinkedList {
+    static class Node{
+        public int val;
+        Node next=null;
+
+        public Node(int val) {
+            this.val = val;
+        }
+        private Node head=null;
+        private Node tail=null;
+        public void offer(int val){
+            Node newNode=new Node(val);
+            if (head==null){
+                head=newNode;
+                tail=newNode;
+                return;
+            }
+//            如果不是空链表
+            tail.next=newNode;
+            tail=tail.next;
+        }
+//        出队列
+        public Integer poll(){
+            if (head==null){
+                return null;
+            }
+            int ret=head.val;
+            head=head.next;
+            if (head==null){
+                    tail= null;
+            }
+            return ret;
+        }
+//        取出首元素
+        public Integer peek(){
+            if (head==null){
+                return null;
+            }
+            return head.val;
+        }
+    }
+}
